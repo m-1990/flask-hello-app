f2e51969cb16a47d109c4ba84e4593e88d30df7d
m-1990 <maham9261@gmail.com>
2025-06-23 01:48:19 +0000

    update workflow with unit test

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+ 
